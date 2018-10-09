6e6b5bb57b7e41d3866b4770829d4261b3fd1120
Leo Chen <chenlt78@qq.com>
2018-10-10 06:14:38 +0800

    Using EC2 role instead of profile
    Signed-off-by: Leo Chen <chenlt78@qq.com>

--- ListProducer/init_env.py
+++ ListProducer/init_env.py
@@ -4,5 +4,5 @@
 from sqs_utils import *
 
 if __name__ == '__main__':
-    sqs_profile = sqsClass(profile_name='dst_profile')
+    sqs_profile = sqsClass()
     sqs_profile.create_test_queues('s3sync-worker', 10)
